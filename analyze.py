40609381e444c714b6624d8b9e01479472038fde
Vamsi Dath <vamsi@Vamsis-MacBook-Air.local>
2025-01-24 16:29:20 -0600

    lab1

---
+++ analyze.py
@@ -0,0 +1,56 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+import time
+
+#Loading environment variables
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#Insted of using the key and endpoint by hardcoding them directly, loading from .env
+endpoint = os.getenv("ENDPOINT")
+key = os.getenv("KEY")
+
+credentials = CognitiveServicesCredentials(key)
+
+client = ComputerVisionClient(
+    endpoint=endpoint,
+    credentials=credentials
+)
+
+def read_image(uri):
+    numberOfCharsInOperationId = 36
+    maxRetries = 10
+
+    # SDK call
+    rawHttpResponse = client.read(uri, language="en", raw=True)
+
+    # Get ID from returned headers
+    operationLocation = rawHttpResponse.headers["Operation-Location"]
+    idLocation = len(operationLocation) - numberOfCharsInOperationId
+    operationId = operationLocation[idLocation:]
+
+    # SDK call
+    result = client.get_read_result(operationId)
+    
+    # Try API
+    retry = 0
+    
+    while retry < maxRetries:
+        if result.status.lower () not in ['notstarted', 'running']:
+            break
+        time.sleep(1)
+        result = client.get_read_result(operationId)
+        
+        retry += 1
+    
+    if retry == maxRetries:
+        return "max retries reached"
+
+    if result.status == OperationStatusCodes.succeeded:
+        res_text = " ".join([line.text for line in result.analyze_result.read_results[0].lines])
+        return res_text
+    else:
+        return "error"
